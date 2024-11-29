d4c082d0cf92976b91263b1df98c5fba440ca5e8
vexe <xarxoohx@gmail.com>
2024-11-28 21:19:28 -0500

    added top, and cross

--- scripts/script.js
+++ scripts/script.js
@@ -80,3 +80,25 @@ document.addEventListener("DOMContentLoaded", () => {
         observer.observe(projectItem);
     });
 });
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction()
+{
+    let mybutton = document.getElementById("top-button");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+    {
+        mybutton.style.display = "block";
+    }
+    else
+    {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
